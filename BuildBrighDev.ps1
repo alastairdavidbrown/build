eca68edad72bf99d3d4075151e69d907b51c708d
Alastair Brown <alastairdavidbrown@gmail.com>
2017-06-06 17:30:32 +0000

    First working version

--- BuildBrighDev.ps1
+++ BuildBrighDev.ps1
@@ -8,6 +8,7 @@ $node_dist = "node-v6.10.3-x64.msi"
 $node_install = "https://nodejs.org/dist/v6.10.3/$node_dist"
 $installs="installs"
 $node_install_out = "$root\$logs\node_install.log"
+$webapp_repo="git@github.com:brighthr/WebApp.git"
 
 
 function build 
@@ -16,6 +17,8 @@ function build
 	New-Item -ItemType directory -Path $root
 	New-Item -ItemType directory -Path $root\$logs
 	New-Item -ItemType directory -Path $root\$installs
+	cd $root
+
 
 	# ------ Install Node
 	Invoke-WebRequest -Uri $node_install -OutFile $root\$installs\$node_dist
@@ -23,6 +26,12 @@ function build
 
 	# ------ Install Grunt
 	Invoke-Expression "npm install grunt-cli grunt"
+
+	# ------ Get the code
+	git clone $webapp_repo 
+
+	# ------ Build the app
+	cd WebApp
 	Invoke-Expression "npm install"
 
 
@@ -30,9 +39,7 @@ function build
 
 function clean 
 {
-	Remove-Item -Path $root\$logs
-	Remove-Item -Path $root\$installs
-	Remove-Item -Path $root	
+	Remove-Item -Force -Recurse -Path $root
 	
 }
 
